6cbb026f0e7afe1fd912dd5660fec129fa799e6e
DannyArends <Danny.Arends@gmail.com>
2014-04-14 17:17:23 +0200

    Updating and reworking files, updates to error checking

--- www/localhost/QR/qrlib.php
+++ www/localhost/QR/qrlib.php
@@ -1,25 +1,20 @@
 <?php
-/*
- * PHP QR Code encoder
- *
- * Root library file, prepares environment and includes dependencies
- *
+/**
+ * PHP QR Code encoder - Root library file, prepares environment and includes dependencies
  */
-	
-	$QR_BASEDIR = dirname(__FILE__).DIRECTORY_SEPARATOR;
-	
-	// Required libs
-	
-	include $QR_BASEDIR."qrconst.php";
-	include $QR_BASEDIR."qrconfig.php";
-	include $QR_BASEDIR."qrtools.php";
-	include $QR_BASEDIR."qrspec.php";
-	include $QR_BASEDIR."qrimage.php";
-    include $QR_BASEDIR."qrvect.php";
-	include $QR_BASEDIR."qrinput.php";
-	include $QR_BASEDIR."qrbitstream.php";
-	include $QR_BASEDIR."qrsplit.php";
-	include $QR_BASEDIR."qrrscode.php";
-	include $QR_BASEDIR."qrmask.php";
-	include $QR_BASEDIR."qrencode.php";
+
+$QR_BASEDIR = dirname(__FILE__).DIRECTORY_SEPARATOR;
+
+include $QR_BASEDIR."qrconst.php";
+include $QR_BASEDIR."qrconfig.php";
+include $QR_BASEDIR."qrtools.php";
+include $QR_BASEDIR."qrspec.php";
+include $QR_BASEDIR."qrimage.php";
+include $QR_BASEDIR."qrvect.php";
+include $QR_BASEDIR."qrinput.php";
+include $QR_BASEDIR."qrbitstream.php";
+include $QR_BASEDIR."qrsplit.php";
+include $QR_BASEDIR."qrrscode.php";
+include $QR_BASEDIR."qrmask.php";
+include $QR_BASEDIR."qrencode.php";
 
