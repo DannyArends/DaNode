e1891ead3a6aef0189539486e6bf99c475a246bd
DannyArends <Danny.Arends@gmail.com>
2017-09-02 11:16:51 +0200

    Client now uses the driver to accept connections

--- danode/client.d
+++ danode/client.d
@@ -30,6 +30,7 @@ interface ClientInterface {
 abstract class DriverInterface {
   public:
     Appender!(char[])   inbuffer;            /// Input appender buffer
+    Socket              serversocket;        /// Server socket for accepting the connection
     Socket              socket;              /// Client socket for reading and writing
     long                requests = 0;        /// Number of requests we handled
     long[long]          senddata;            /// Size of data send per request
@@ -37,6 +38,7 @@ abstract class DriverInterface {
     SysTime             modtime;             /// Time in ms since this process was last modified
     Address             address;             /// Private address field
 
+    bool openConnection();
     ptrdiff_t receive(Socket conn, ptrdiff_t maxsize = 4096);
     void send(ref Response response, Socket conn, ptrdiff_t maxsize = 4096);
     bool isSecure();
@@ -57,44 +59,51 @@ class Client : Thread, ClientInterface {
       super(&run);
     }
 
-   final void run(){
+   final void run() {
       if(router.verbose >= DEBUG) writefln("[DEBUG]  new connection established %s:%d", ip(), port() );
-      try{
+      try {
+        if(this.driver.openConnection() == false) return;
         Request request;
         Response response;
-        while(running && modified < maxtime){
-          if(driver.receive(driver.socket) > 0){                                                // We've received new data
-            if(!response.ready){                                                                // If we're not ready to respond yet
-              router.route(ip(), port(), request, response, to!string(driver.inbuffer.data), driver.isSecure());   // Parse the data and try to create a response (Could fail multiple times)
+        while(running && modified < maxtime) {
+          if(driver.receive(driver.socket) > 0){                      // We've received new data
+            if(!response.ready){                                      // If we're not ready to respond yet
+              // Parse the data and try to create a response (Could fail multiple times)
+              router.route(ip(), port(), request, response, to!string(driver.inbuffer.data), driver.isSecure());
             }
-            if(response.ready && !response.completed){                                        // We know what to respond, but haven't send all of it yet
-              driver.send(response, driver.socket);                                           // Send the response, hit multiple times, send what you can and return
+            if(response.ready && !response.completed){                          // We know what to respond, but haven't send all of it yet
+              driver.send(response, driver.socket);                             // Send the response, hit multiple times, send what you can and return
             }
-            if(response.ready && response.completed){                               // We've completed the request, response cycle
-              router.logrequest(this, request, response);                           // Log the response to the request
-              request.clearUploadFiles();                                           // Remove any upload files left over
-              request.destroy();                                                    // Clear the request and uploaded files
-              driver.inbuffer.destroy();                                            // Clear the input buffer
+            if(response.ready && response.completed){                           // We've completed the request, response cycle
+              router.logrequest(this, request, response);                       // Log the response to the request
+              request.clearUploadFiles();                                       // Remove any upload files left over
+              request.destroy();                                                // Clear the request and uploaded files
+              driver.inbuffer.destroy();                                        // Clear the input buffer
               driver.requests++;
-              if(!response.keepalive) stop();                                       // No keep alive, then stop this client
-              response.destroy();                                                   // Clear the response
+              if(!response.keepalive) stop();                                   // No keep alive, then stop this client
+              response.destroy();                                               // Clear the response
             }
 
-          }else{
+          } else {
             Thread.sleep(dur!"msecs"(1));
           }
           // writefln("[INFO]   connection %s:%s (%s msecs) %s", ip, port, Msecs(driver.starttime), to!string(driver.inbuffer.data));
           Thread.yield();
         }
-      }catch(Exception e){ writefln("[WARN]   unknown client exception: %s", e.msg); }
+      } catch(Exception e) { 
+        writefln("[WARN]   unknown client exception: %s", e.msg);
+      }
       if(router.verbose >= INFO){
         writefln("[INFO]   connection %s:%s (%s) closed after %d requests %s (%s msecs)", ip, port, (driver.isSecure() ? "⚐" : "⚑"), 
                                                                                           driver.requests, driver.senddata, Msecs(driver.starttime));
       }
-      driver.socket.close();
+      if(driver.socket !is null) {
+        driver.socket.close();
+      }
     }
 
     final @property bool running() {
+      if(driver.socket is null) return(false);
       return(driver.socket.isAlive() && !terminated);
     }
 
@@ -127,17 +136,31 @@ class Client : Thread, ClientInterface {
 }
 
 class HTTP : DriverInterface {
+  private:
+    bool blocking = false;
+
   public:
     this(Socket socket, bool blocking = false){ // writefln("[HTTP]   driver constructor");
-      this.socket = socket;
-      this.socket.blocking = blocking;
-      this.starttime = Clock.currTime();           /// Time in ms since this process came alive
+      this.serversocket = socket;
+      this.blocking = blocking;
+      this.starttime = Clock.currTime();         /// Time in ms since this process came alive
       this.modtime = Clock.currTime();           /// Time in ms since this process was modified
+    }
+
+    override bool openConnection() {
       try {
-        this.address        = socket.remoteAddress();
+        this.socket = this.serversocket.accept();      // Accept the incoming socket connection
+        this.socket.blocking = this.blocking;
       } catch(Exception e) {
-        writefln("[WARN]   unable to resolve requesting origin");
+        writefln("[ERROR]  unable to accept socket: %s", e.msg);
+        return(false);
       }
+      try {
+        this.address = socket.remoteAddress();
+      } catch(Exception e) {
+        writefln("[WARN]   unable to resolve requesting origin: %s", e.msg);
+      }
+      return(true);
     }
 
     override ptrdiff_t receive(Socket socket, ptrdiff_t maxsize = 4096) {
@@ -151,7 +174,7 @@ class HTTP : DriverInterface {
       return(inbuffer.data.length);
     }
 
-    override void send(ref Response response, Socket socket, ptrdiff_t maxsize = 4096) {
+    override void send(ref Response response, Socket socket, ptrdiff_t maxsize = 4096)  { synchronized {
       if(!socket.isAlive()) return;
       ptrdiff_t send = socket.send(response.bytes(maxsize));
       if(send >= 0) {
@@ -159,7 +182,7 @@ class HTTP : DriverInterface {
         if(response.index >= response.length) response.completed = true;
       }
       // if(send > 0) writefln("[INFO]   send %d bytes of data", send);
-    }
+    } }
 
     override bool isSecure(){ return(false); }
 }
