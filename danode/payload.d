21a7d5fb52f628f0a601d18f07cf11f5e603285c
DannyArends <Danny.Arends@gmail.com>
2017-09-09 12:03:49 +0200

    Handling the status-codes and parsing of the header from CGI scripts

--- danode/payload.d
+++ danode/payload.d
@@ -35,44 +35,67 @@ class CGI : Payload {
 
     final @property PayLoadType   type() const { return(PayLoadType.Script); }
     final @property long          ready() { return(external.finished); }
-    final @property ptrdiff_t     length() const { 
+    final @property ptrdiff_t     length() const {
       if(!external.running) return(getHeader!ptrdiff_t("Content-Length", to!ptrdiff_t(external.length)));
       return -1; 
     }
     final @property SysTime       mtime() { return Clock.currTime(); }
     final @property string        mimetype() const { return "text/html"; } // Todo if there is a header parse it out of there
 
-    final T getHeader(T)(string key, T def = T.init, ptrdiff_t i = 1) const {
+    final T getHeader(T)(string key, T def = T.init) const {
       if(endOfHeader > 0){
-        foreach(line; to!string(external.output(0))[0..endOfHeader()].split("\n")){
+        foreach(line; fullHeader().split("\n")){
           string[] elems = line.split(": ");
-          if(elems.length >= (i+1) && toLower(elems[0]) == toLower(key)) return to!T(strip(elems[i].split(" ")[0]));
+          if(elems.length == 2) {
+            if(toLower(elems[0]) == toLower(key)) return to!T(strip(elems[1]));
+          }
         }
       }
       return(def);
     }
 
-    @property final HeaderType headerType() {
+    @property final HeaderType headerType() const {
       if(endOfHeader() <= 0) return HeaderType.None;
-      string respl = fullHeader().split("\n")[0];
-      string[] values = respl.split(" ");
+      string[] values = firstHeaderLine().split(" ");
       if(values.length >= 3 && values[0] == "HTTP/1.0") return HeaderType.HTTP10;
       if(values.length >= 3 && values[0] == "HTTP/1.1") return HeaderType.HTTP11;
       if(getHeader("Status", "") != "") return HeaderType.FastCGI;
       return HeaderType.None;
     }
 
-    @property final string fullHeader() {
-      return(to!string( bytes(0, endOfHeader()) ));
+    @property final string fullHeader() const {
+      string outputSoFar = to!string(external.output(0));
+      return outputSoFar[0 .. endOfHeader()];
+    }
+
+    @property final string firstHeaderLine() const {
+      string outputSoFar = to!string(external.output(0));
+      return(outputSoFar[0 .. outputSoFar.indexOf("\n")]);
     }
 
     @property final StatusCode statuscode() const {
-      long status = getHeader("status", -1);
-      if(status == -1) return((external.status == 0)? StatusCode.Ok : StatusCode.ISE );
-      return(to!StatusCode(to!int(status)));
+      string status = "";
+      if(headerType() == HeaderType.FastCGI) {
+        status = getHeader!string("Status", ""); // Fast-CGI provides: "Status: Code Reason"
+        status = status.split(" ")[0];
+      }
+      if(headerType() == HeaderType.HTTP10 || headerType() == HeaderType.HTTP11) {
+        string[] values = firstHeaderLine().split(" ");
+        if(values.length >= 3) status = values[1];
+      }
+      if(status == "") return((external.status == 0)? StatusCode.Ok : StatusCode.ISE );
+      StatusCode s = StatusCode.ISE;
+      try {
+        s = to!StatusCode(to!int(status));
+      } catch (Exception e){
+        writeln("[ERROR]  Unable to get statuscode from script");
+      }
+      return(s);
     }
 
-    const(char)[] bytes(ptrdiff_t from, ptrdiff_t maxsize = 1024){ return(external.output(from)[0 .. to!ptrdiff_t(fmin(from+maxsize, $))]); }
+    const(char)[] bytes(ptrdiff_t from, ptrdiff_t maxsize = 1024) {
+      return(external.output(from)[0 .. to!ptrdiff_t(fmin(from+maxsize, $))]);
+    }
 
     final ptrdiff_t endOfHeader() const {
       string outputSoFar = to!string(external.output(0));
