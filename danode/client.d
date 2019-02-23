79b9a210fc31c8dd765a206fa68dbd0f8f8a1696
DannyArends <Danny.Arends@gmail.com>
2019-02-23 12:33:33 +0100

    Check if the client is terminated first

--- danode/client.d
+++ danode/client.d
@@ -29,9 +29,8 @@ class Client : Thread, ClientInterface {
       if (router.verbose >= DEBUG) writefln("[DEBUG]  new connection established %s:%d", ip(), port() );
       try {
         if (driver.openConnection() == false) {
-          writefln("[WARN]  new connection aborted: unable to open connection");
+          writefln("[WARN]   new connection aborted: unable to open connection");
           terminated = true;
-          return;
         }
         scope (exit) {
           if (driver.isAlive()) driver.closeConnection();
@@ -76,7 +75,7 @@ class Client : Thread, ClientInterface {
 
     final @property bool running() {
       if (driver.socket is null) return(false);
-      return(driver.socket.isAlive() && !terminated);
+      return(!terminated && driver.socket.isAlive());
     }
 
     final @property long starttime(){
