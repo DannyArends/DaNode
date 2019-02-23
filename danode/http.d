62ef8a1cd156a357bcd78734d9e3780310e48cb6
DannyArends <Danny.Arends@gmail.com>
2019-02-23 18:39:00 +0100

    More comments, SSL handshake into it's own function

--- danode/http.d
+++ danode/http.d
@@ -10,10 +10,11 @@ class HTTP : DriverInterface {
     this(Socket socket, bool blocking = false, int verbose = NORMAL) { // writefln("[HTTP]   driver constructor");
       this.socket = socket;
       this.blocking = blocking;
-      this.starttime = Clock.currTime();         /// Time in ms since this process came alive
-      this.modtime = Clock.currTime();           /// Time in ms since this process was modified
+      this.starttime = Clock.currTime(); // Time in ms since this process came alive
+      this.modtime = Clock.currTime(); // Time in ms since this process was modified
     }
 
+    // Open the connection by setting the socket to non blocking I/O, and registering the origin address
     override bool openConnection() {
       try {
         socket.blocking = this.blocking;
@@ -29,30 +30,34 @@ class HTTP : DriverInterface {
       return(true);
     }
 
+    // Receive upto maxsize of bytes from the client into the input buffer
     override ptrdiff_t receive(Socket socket, ptrdiff_t maxsize = 4096) {
+      if(socket is null) return(-1);
+      if(!socket.isAlive()) return(-1);
       ptrdiff_t received;
       char[] tmpbuffer = new char[](maxsize);
-      if(!socket.isAlive()) return(-1);
-      if((received = socket.receive(tmpbuffer)) > 0) {
+      if ((received = socket.receive(tmpbuffer)) > 0) {
         inbuffer.put(tmpbuffer[0 .. received]); modtime = Clock.currTime();
       }
       // if(received > 0) writefln("[INFO]   received %d bytes of data", received);
       return(inbuffer.data.length);
     }
 
+    // Send upto maxsize bytes from the response to the client
     override void send(ref Response response, Socket socket, ptrdiff_t maxsize = 4096)  { synchronized {
       if(socket is null) return;
       if(!socket.isAlive()) return;
       ptrdiff_t send = socket.send(response.bytes(maxsize));
-      if(send >= 0) {
-        if(send > 0) modtime = Clock.currTime();
+      if (send >= 0) {
+        if (send > 0) modtime = Clock.currTime();
         response.index += send; senddata[requests] += send;
         if(response.index >= response.length) response.completed = true;
       }
       // if(send > 0) writefln("[INFO]   send %d bytes of data", send);
     } }
 
-    override void closeConnection() {
+    // Close the connection, by shutting down the socket
+    override void closeConnection() nothrow {
       if (socket !is null) {
         try {
           socket.shutdown(SocketShutdown.BOTH);
@@ -63,8 +68,9 @@ class HTTP : DriverInterface {
       }
     }
 
+    // Is the connection alive ?, make sure we check for null
     override bool isAlive() { 
-      if(socket !is null) { return socket.isAlive(); }
+      if (socket !is null) return(socket.isAlive());
       return false;
     }
 
--- danode/https.d
+++ danode/https.d
@@ -20,11 +20,31 @@ version(SSL) {
       this(Socket socket, bool blocking = false, int verbose = NORMAL) {
         this.socket = socket;
         this.blocking = blocking;
-        this.starttime = Clock.currTime(); /// Time in ms since this process came alive
-        this.modtime = Clock.currTime(); /// Time in ms since this process was modified
+        this.starttime = Clock.currTime(); // Time in ms since this process came alive
+        this.modtime = Clock.currTime(); // Time in ms since this process was modified
         if(verbose >= INFO) writeln("[HTTPS]  HTTPS driver initialized");
       }
 
+      // Perform the SSL handshake
+      bool performHandshake() {
+        bool handshaked = false;
+        int ret_accept, ret_error;
+        while (!handshaked && Msecs(starttime) < 500) {
+          ret_accept = SSL_accept(ssl);
+          if (ret_accept == 1) {
+            handshaked = true;
+          } else {
+            ret_error = ssl.checkForError(socket, ret_accept);
+            if (ret_accept == 0) return(false);
+            if (ret_error == SSL_ERROR_SSL) return(false);
+            if (ret_error == SSL_ERROR_WANT_READ) Thread.sleep(5.msecs);
+            if (ret_error == SSL_ERROR_WANT_WRITE) Thread.sleep(5.msecs);
+          }
+        }
+        return(handshaked);
+      }
+
+      // Open the connection by setting the socket to non blocking I/O, and registering the origin address
       override bool openConnection() { synchronized {
         if(verbose >= INFO) writeln("[HTTPS]  Opening HTTPS connection");
         if (ncontext > 0) {
@@ -47,24 +67,12 @@ version(SSL) {
             if(verbose >= INFO) writefln("[HTTPS]  SSL_set_accept_state");
             SSL_set_accept_state(this.ssl);
 
-            bool handshaked = false;
-            int ret_accept, ret_error;
-            while (!handshaked && Msecs(starttime) < 400) {
-              ret_accept = SSL_accept(ssl);
-              if (ret_accept == 1) {
-                handshaked = true;
-              } else {
-                ret_error = ssl.checkForError(socket, ret_accept);
-                if (ret_accept == 0) return(false);
-                if(ret_error == SSL_ERROR_WANT_READ) Thread.sleep(5.msecs);
-                if(ret_error == SSL_ERROR_SSL) { closeConnection(); return(false); }
-              }
-            }
-            if(!handshaked) {
-              writefln("[ERROR]  couldn't handshake SSL connection : %d|%d", ret_accept, ret_error);
+            bool handshaked = performHandshake();
+            if (!handshaked) {
+              writefln("[ERROR]  couldn't handshake SSL connection");
               return(false);
             }
-          } catch(Exception e) {
+          } catch (Exception e) {
             writefln("[ERROR]  couldn't open SSL connection : %s", e.msg);
             return(false);
           }
@@ -72,7 +80,7 @@ version(SSL) {
             if (this.socket !is null) {
               this.address = this.socket.remoteAddress();
             }
-          } catch(Exception e) {
+          } catch (Exception e) {
             if(verbose >= INFO) writefln("[WARN]   unable to resolve requesting origin: %s", e.msg);
           }
           if(verbose >= INFO) writeln("[HTTPS]  HTTPS connection opened");
@@ -83,6 +91,7 @@ version(SSL) {
         return(false);
       } }
 
+      // Close the connection, by shutting down the SSL and Socket object
       override void closeConnection() { synchronized {
         if (socket !is null) {
           try {
@@ -95,11 +104,13 @@ version(SSL) {
         }
       } }
 
+      // Is the connection alive ?, make sure we check for null
       override bool isAlive() { 
-        if(socket !is null) { return socket.isAlive(); }
+        if(socket !is null) return socket.isAlive();
         return false;
       }
 
+      // Receive upto maxsize of bytes from the client into the input buffer
       override ptrdiff_t receive(Socket socket, ptrdiff_t maxsize = 4096){ synchronized {
         ptrdiff_t received;
         if(socket is null) return -1;
@@ -112,6 +123,7 @@ version(SSL) {
         return(inbuffer.data.length);
       } }
 
+      // Send upto maxsize bytes from the response to the client
       override void send(ref Response response, Socket socket, ptrdiff_t maxsize = 4096){ synchronized {
         auto slice = response.bytes(maxsize);
         if(socket is null) return;
@@ -128,4 +140,8 @@ version(SSL) {
       override bool isSecure() { return(true); }
   }
 
+  unittest {
+    writefln("[FILE]   %s", __FILE__);
+  }
 }
+
