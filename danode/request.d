08d3781b155e7465aaaa5db8804161223537dced
DannyArends <Danny.Arends@gmail.com>
2019-03-02 14:32:14 +0100

    Comments and code cleaning

--- danode/request.d
+++ danode/request.d
@@ -21,17 +21,18 @@ SysTime parseHtmlDate(const string datestr){ // 21 Apr 2014 20:20:13 CET
 
 struct Request {
   UUID              requestid; ///md5UUID for this request
-  DriverInterface   driver;
-  string            method = "GET";
-  string            uri = "/";
-  string            url = "/";
-  string            page;         // Used when redirecting
-  string            dir;          // Used when redirecting
-  string            protocol = "HTTP/1.1";
-  string[string]    headers;
-  SysTime           starttime;
-  PostItem[string]  postinfo;
+  DriverInterface   driver; /// DriverInterface associated with the Request object
+  string            method = "GET"; /// requested HTTP method (GET, POST, HEAD)
+  string            uri = "/"; /// uri requested
+  string            url = "/"; /// url requested
+  string            page; /// page is used when redirecting
+  string            dir; /// dir is used in directory redirection
+  string            protocol = "HTTP/1.1"; /// protocol requested
+  string[string]    headers; /// Associative array holding the header values
+  SysTime           starttime; /// start time of the Request
+  PostItem[string]  postinfo;  /// Associative array holding the post parameters and values
 
+  // Start a new Request, and parseHeader on the DriverInterface
   final void parse(DriverInterface driver) {
     this.driver = driver;
     this.starttime = Clock.currTime();
@@ -41,6 +42,7 @@ struct Request {
     trace("request header: %s", driver.header);
   }
 
+  // Parse the HTML request header (method, uri, protocol) as well as the supplemental headers
   final void parseHeader(in string header) {
     string[] parts;
     foreach(i, line; header.split("\n")){
@@ -60,34 +62,41 @@ struct Request {
     }
   }
 
-  final void update(DriverInterface driver){ this.driver = driver; }
+  // New input was obtained and / or the driver has been changed, update the driver
+  final void update(DriverInterface driver) { this.driver = driver; }
 
+  // The Host header requested in the request
   final @property string host() const { 
     ptrdiff_t i = headers.from("Host").indexOf(":");
     if(i > 0) return(headers.from("Host")[0 .. i]);
     return(headers.from("Host")); 
   }
 
+  // The Post from the Host header in the request
   final @property ushort serverport() const {
     ptrdiff_t i = headers.from("Host").indexOf(":");
     if(i > 0){ return( to!ushort(headers.from("Host")[(i+1) .. $])); } 
     return(to!ushort(80));
   }
 
+  // Input file generated storing the headers of the request
   final @property string inputfile(in FileSystem filesystem) const {
     return format("%s/%s.in", filesystem.localroot(shorthost()), this.requestid);
   }
 
+  // Location of a file with name, uploaded by POST request
   final @property string uploadfile(in FileSystem filesystem, in string name) const {
     return format("%s/%s.up", filesystem.localroot(shorthost()), md5UUID(format("%s:%d-%s-%s", driver.ip, driver.port, starttime, name)));
   }
 
+  // Get parameters as associative array
   final string[string] get() const {
     string[string] params;
     foreach(param; query[1 .. $].split("&")){ string[] elems = param.split("="); if(elems.length == 1){ elems ~= "TRUE"; } params[elems[0]] = elems[1]; }
     return params;
   }
 
+  // List of filenames uploaded by the user
   final @property string[]  postfiles() const { 
     string[] files;
     foreach (p; postinfo) {
@@ -109,13 +118,15 @@ struct Request {
   final string              shorthost() const { return( (host.indexOf("www.") >= 0)? host[4 .. $] : host ); }
   final string              command(string localpath) const { return(format("%s %s%s", localpath.interpreter(), localpath, params())); }
 
+  // Canonical redirect of the Request for a directory to the index page specified in the WebConfig
   final void redirectdir(in WebConfig config) {
     if(config.redirectdir() && config.redirect){
-      this.dir = this.path()[1..$];   // Save the URL path
+      this.dir = this.path()[1..$]; // We need to redirect, so save the path to this.dir
       this.url = config.index;
     }
   }
 
+  // Clear all files uploaded by the user after the Request is done
   final void clearUploadFiles() const {
     foreach(f; postfiles) { if(exists(f)) {
       trace("removing uploaded file at %s", f); 
