b0e817f0d5ff8c9e9aadbc5ec10ee9fbb39c4116
DannyArends <Danny.Arends@gmail.com>
2019-03-02 11:54:51 +0100

    Using the logic from the driver

--- danode/client.d
+++ danode/client.d
@@ -1,7 +1,6 @@
 module danode.client;
 
 import danode.imports;
-import danode.functions : Msecs;
 import danode.router : Router;
 import danode.httpstatus : StatusCode;
 import danode.interfaces : DriverInterface, ClientInterface;
@@ -31,7 +30,7 @@ class Client : Thread, ClientInterface {
       try {
         if (driver.openConnection() == false) {
           warning("new connection aborted: unable to open connection");
-          terminated = true;
+          stop();
         }
         scope (exit) {
           if (driver.isAlive()) driver.closeConnection();
@@ -39,69 +38,59 @@ class Client : Thread, ClientInterface {
         Request request;
         Response response;
         while (running) {
-          if (driver.receive(driver.socket) > 0) {                      // We've received new data
-            if (!response.ready) {                                      // If we're not ready to respond yet
+          if (driver.receive(driver.socket) > 0) {     // We've received new data
+            if (!response.ready) {                            // If we're not ready to respond yet
               // Parse the data and try to create a response (Could fail multiple times)
               router.route(ip(), port(), request, response, to!string(driver.inbuffer.data), driver.isSecure());
             }
-            if (response.ready && !response.completed) {                        // We know what to respond, but haven't send all of it yet
-              driver.send(response, driver.socket);                             // Send the response, hit multiple times, send what you can and return
+            if (response.ready && !response.completed) {      // We know what to respond, but haven't send all of it yet
+              driver.send(response, driver.socket);           // Send the response, hit multiple times, send what you can and return
             }
-            if (response.ready && response.completed) {                         // We've completed the request, response cycle
-              router.logrequest(this, request, response);                       // Log the response to the request
-              request.clearUploadFiles();                                       // Remove any upload files left over
-              request.destroy();                                                // Clear the request structure
-              driver.inbuffer.destroy();                                        // Clear the input buffer
+            if (response.ready && response.completed) {       // We've completed the request, response cycle
+              router.logRequest(this, request, response);     // Log the response to the request
+              request.clearUploadFiles();                     // Remove any upload files left over
+              request.destroy();                              // Clear the request structure
+              driver.inbuffer.destroy();                      // Clear the input buffer
               driver.requests++;
-              if(!response.keepalive) stop();                                   // No keep alive, then stop this client
-              response.destroy();                                               // Clear the response structure
+              if(!response.keepalive) stop();                 // No keep alive, then stop this client
+              response.destroy();                             // Clear the response structure
             }
-          } else {
+          } else { // No data was received
             Thread.sleep(dur!"msecs"(1));
           }
-          if(lastmodified >= maxtime) terminated = true;
-          custom(3, "CLIENT", "connection %s:%s (%s msecs) %s", ip, port, Msecs(driver.starttime), to!string(driver.inbuffer.data));
+          if (lastmodified >= maxtime) { // Client are not allowed to be silent for more than maxtime
+            warning("client stopped due to maxtime limit: %s > %s", lastmodified, maxtime);
+            stop();
+          }
+          custom(3, "CLIENT", "connection %s:%s (%s msecs) %s", ip, port, starttime, to!string(driver.inbuffer.data));
           Thread.yield();
         }
       } catch(Exception e) { 
         warning("unknown client exception: %s", e.msg);
-        terminated = true;
+        stop();
       }
       custom(1, "CLIENT", "connection %s:%s (%s) closed after %d requests %s (%s msecs)", ip, port, (driver.isSecure() ? "⚐" : "⚑"), 
-                                                                                          driver.requests, driver.senddata, Msecs(driver.starttime));
+                                                                                          driver.requests, driver.senddata, starttime);
       driver.destroy();                                               // Clear the response structure
     }
 
-    final @property bool running() {
+    final @property bool running() const {
       if (driver.socket is null) return(false);
       return(!terminated && driver.socket.isAlive());
     }
 
-    final @property long starttime(){
-      return(Msecs(driver.starttime));
-    }
-
-    final @property long lastmodified(){
-      return(Msecs(driver.modtime));
-    }
-
-    final @property void stop(){
+    final @property void stop() {
       trace("connection %s:%s stop called", ip, port);
       terminated = true; 
     }
 
-    final @property long port() const { 
-      if (driver.address !is null) {
-        return(to!long(driver.address.toPortString())); 
-      } 
-      return(-1); 
-    }
+    final @property long starttime() const { return(driver.starttime); }
+    final @property long lastmodified() const { return(driver.lastmodified); }
+    final @property long port() const { return(driver.port()); } 
+    final @property string ip() const { return(driver.ip()); } 
+}
 
-    final @property string ip() const {
-      if (driver.address !is null) {
-        return(driver.address.toAddrString()); 
-      }
-      return("0.0.0.0"); 
-    }
+unittest {
+  custom(0, "FILE", "%s", __FILE__);
 }
 
