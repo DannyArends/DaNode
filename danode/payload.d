33947f91e52bdb0b70102673c034ce3265341c0f
DannyArends <Danny.Arends@gmail.com>
2019-03-06 08:14:13 +0100

    Code linting

--- danode/payload.d
+++ danode/payload.d
@@ -1,10 +1,8 @@
 module danode.payload;
 
 import danode.imports;
-import danode.process : Process;
 import danode.httpstatus : StatusCode;
-import danode.mimetypes : UNSUPPORTED_FILE, mime;
-import danode.log : error;
+import danode.mimetypes : UNSUPPORTED_FILE;
 
 enum PayloadType { Message, Script, File }
 enum HeaderType { None, FastCGI, HTTP10, HTTP11 }
@@ -28,19 +26,27 @@ class Message : Payload {
     string mime;
 
   public:
-    this(StatusCode status, string message, string mime = "text/plain"){ this.status = status; this.message = message; this.mime = mime; }
+    this(StatusCode status, string message, string mime = "text/plain") {
+      this.status = status;
+      this.message = message;
+      this.mime = mime;
+    }
 
-    final @property PayloadType   type() const { return(PayloadType.Message); }
-    final @property long          ready() { return(true); }
-    final @property ptrdiff_t     length() const { return(message.length); }
-    final @property SysTime       mtime() { return Clock.currTime(); }
-    final @property string        mimetype() const { return mime; }
-    final @property StatusCode    statuscode() const { return status; }
-    char[] bytes(ptrdiff_t from, ptrdiff_t maxsize = 1024){ return( message[from .. to!ptrdiff_t(fmin(from+maxsize, $))].dup ); }
+    final @property PayloadType type() const { return(PayloadType.Message); }
+    final @property long ready() { return(true); }
+    final @property ptrdiff_t length() const { return(message.length); }
+    final @property SysTime mtime() { return Clock.currTime(); }
+    final @property string mimetype() const { return mime; }
+    final @property StatusCode statuscode() const { return status; }
+    char[] bytes(ptrdiff_t from, ptrdiff_t maxsize = 1024) { 
+      return( message[from .. to!ptrdiff_t(fmin(from+maxsize, $))].dup );
+    }
 }
 
 class Empty : Message {
   public:
-    this(StatusCode status, string mime = UNSUPPORTED_FILE){ super(status, "", mime); }
+    this(StatusCode status, string mime = UNSUPPORTED_FILE) {
+      super(status, "", mime);
+    }
 }
 
