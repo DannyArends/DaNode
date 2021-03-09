fca206dbfe8990a894a9fb460a0df9d1d5aedc15
DannyArends <Danny.Arends@Gmail.com>
2021-03-09 12:27:37 +0100

    Only output broken pipe signals when trace mode is enabled

--- danode/signals.d
+++ danode/signals.d
@@ -9,10 +9,10 @@ version(Posix) {
   extern(C) @nogc nothrow void handle_signal(int signal) {
     switch (signal) {
       case SIGPIPE:
-        if(cverbose) write(2, cast(const(void*)) "[SIG]    Broken pipe caught, and ignored\n\0".ptr, 41);
+        if(cverbose > 1) write(2, cast(const(void*)) "[SIG]    Broken pipe caught, and ignored\n\0".ptr, 41);
         break;
       default:
-        if(cverbose) write(2, cast(const(void*)) "[SIG]    Caught\n\0".ptr, 17);
+        if(cverbose > 1) write(2, cast(const(void*)) "[SIG]    Caught\n\0".ptr, 17);
         break;
     }
   }
