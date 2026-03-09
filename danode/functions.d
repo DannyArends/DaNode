90918e300ae50e71e8e424dcd666b268f650c65a
DannyArends <Danny.Arends@gmail.com>
2026-03-09 12:35:41 +0000

    Fixing safePath

--- danode/functions.d
+++ danode/functions.d
@@ -38,7 +38,7 @@ pure string htmlEscape(string s) {
 string safePath(in string root, in string path) {
   if (path.canFind("..")) return null;
   string relPath = path.startsWith("/") ? path[1 .. $] : path;
-  return buildNormalizedPath(root, relPath);  // keep relative, same format as before
+  return(root ~ "/" ~ relPath);
 }
 
 // Month to index of the year
